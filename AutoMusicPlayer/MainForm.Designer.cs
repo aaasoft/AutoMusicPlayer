befca63da32f20bb5296ee7331c48513a832e162
张鹏 <scbeta@qq.com>
2022-08-28 21:16:41 +0800

    First commit.

---
+++ AutoMusicPlayer/MainForm.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace AutoMusicPlayer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbMain = new System.Windows.Forms.ProgressBar();
+            this.lblProgress = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerUDiskCheck = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblUDisk = new System.Windows.Forms.Label();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblArtist = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAlbum = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pbMain
+            // 
+            this.pbMain.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbMain.Location = new System.Drawing.Point(35, 255);
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(827, 31);
+            this.pbMain.TabIndex = 4;
+            // 
+            // lblProgress
+            // 
+            this.lblProgress.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblProgress.Location = new System.Drawing.Point(143, 211);
+            this.lblProgress.Name = "lblProgress";
+            this.lblProgress.Size = new System.Drawing.Size(719, 32);
+            this.lblProgress.TabIndex = 5;
+            this.lblProgress.Text = "00:00 / 00:00";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "时间：";
+            // 
+            // timerUDiskCheck
+            // 
+            this.timerUDiskCheck.Enabled = true;
+            this.timerUDiskCheck.Interval = 1000;
+            this.timerUDiskCheck.Tick += new System.EventHandler(this.timerUDiskCheck_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "U盘：";
+            // 
+            // lblUDisk
+            // 
+            this.lblUDisk.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblUDisk.Location = new System.Drawing.Point(143, 9);
+            this.lblUDisk.Name = "lblUDisk";
+            this.lblUDisk.Size = new System.Drawing.Size(719, 32);
+            this.lblUDisk.TabIndex = 5;
+            // 
+            // btnPause
+            // 
+            this.btnPause.Enabled = false;
+            this.btnPause.Location = new System.Drawing.Point(35, 301);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(114, 59);
+            this.btnPause.TabIndex = 6;
+            this.btnPause.Text = "暂停";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPre
+            // 
+            this.btnPre.Enabled = false;
+            this.btnPre.Location = new System.Drawing.Point(155, 301);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(114, 59);
+            this.btnPre.TabIndex = 6;
+            this.btnPre.Text = "上一曲";
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(275, 301);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(114, 59);
+            this.btnNext.TabIndex = 6;
+            this.btnNext.Text = "下一曲";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "艺术家：";
+            // 
+            // lblArtist
+            // 
+            this.lblArtist.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblArtist.Location = new System.Drawing.Point(143, 51);
+            this.lblArtist.Name = "lblArtist";
+            this.lblArtist.Size = new System.Drawing.Size(719, 32);
+            this.lblArtist.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "专辑：";
+            // 
+            // lblAlbum
+            // 
+            this.lblAlbum.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAlbum.Location = new System.Drawing.Point(143, 94);
+            this.lblAlbum.Name = "lblAlbum";
+            this.lblAlbum.Size = new System.Drawing.Size(719, 32);
+            this.lblAlbum.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 137);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 24);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "标题：";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTitle.Location = new System.Drawing.Point(143, 136);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(719, 32);
+            this.lblTitle.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 398);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPre);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblAlbum);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblArtist);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblUDisk);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblProgress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbMain);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自动音乐播放器";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ProgressBar pbMain;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timerUDiskCheck;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblUDisk;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblArtist;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblAlbum;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
+
---
+++ AutoMusicPlayer/MainForm.cs
@@ -0,0 +1,243 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AutoMusicPlayer
+{
+    public partial class MainForm : Form
+    {
+        private MediaPlayer mediaPlayer;
+        private LibVLC libvlc;
+        private DriveInfo currentDriverInfo;
+        private int currentMediaIndex = 0;
+        private List<string> mediaList = new List<string>();
+        private System.Speech.Synthesis.SpeechSynthesizer speechSynthesizer;
+        private string[] musicFileSearchPatterns = new[] { "*.mp3", "*.flac", "*.wma" };
+
+        public MainForm()
+        {
+            InitializeComponent();
+            speechSynthesizer = new System.Speech.Synthesis.SpeechSynthesizer();
+            libvlc = new LibVLC(enableDebugLogs: true);
+            mediaPlayer = new MediaPlayer(libvlc);
+            mediaPlayer.Playing += MediaPlayer_Playing;
+            mediaPlayer.Paused += MediaPlayer_Paused;
+            mediaPlayer.TimeChanged += Mediaplayer_TimeChanged;
+            mediaPlayer.EndReached += MediaPlayer_EndReached;
+            mediaPlayer.EncounteredError += MediaPlayer_EncounteredError;
+            speechSynthesizer.Speak("自动音乐播放器启动完成。");
+        }
+
+        private bool HasChinese(string str)
+        {
+            return Regex.IsMatch(str, @"[\u4e00-\u9fa5]");
+        }
+
+        private void MediaPlayer_Playing(object sender, EventArgs e)
+        {
+            BeginInvoke(new Action(() =>
+            {
+                btnPause.Text = "暂停";
+                lblArtist.Text = mediaPlayer.Media.Meta(MetadataType.Artist);
+                lblAlbum.Text = mediaPlayer.Media.Meta(MetadataType.Album);
+                lblTitle.Text = mediaPlayer.Media.Meta(MetadataType.Title);
+                var title = mediaPlayer.Media.Meta(MetadataType.Title);
+                if (HasChinese(title))
+                    speechSynthesizer.Speak("正在播放：" + lblTitle.Text);
+            }));
+        }
+
+        private void MediaPlayer_Paused(object sender, EventArgs e)
+        {
+            BeginInvoke(new Action(() =>
+            {
+                btnPause.Text = "播放";
+            }));
+        }
+
+        private void MediaPlayer_EncounteredError(object sender, EventArgs e)
+        {
+            playNext();
+        }
+
+        private void MediaPlayer_EndReached(object sender, EventArgs e)
+        {
+            BeginInvoke(new Action(() =>
+            {
+                playNext();
+            }));
+        }
+
+        private void onError()
+        {
+            BeginInvoke(new Action(() =>
+            {
+                currentDriverInfo = null;
+                currentMediaIndex = 0;
+                mediaList.Clear();
+
+                pbMain.Value = 0;
+                lblProgress.Text = string.Empty;
+                lblUDisk.Text = null;
+                lblArtist.Text = null;
+                lblAlbum.Text = null;                
+                lblTitle.Text = null;
+                btnPause.Enabled = false;
+                btnPre.Enabled = false;
+                btnNext.Enabled = false;
+            }));
+        }
+
+        private void Mediaplayer_TimeChanged(object sender, MediaPlayerTimeChangedEventArgs e)
+        {
+            var totalTime = mediaPlayer.Media.Duration;
+            if (totalTime == 0)
+            {
+                onError();
+                return;
+            }
+
+            var progress = Convert.ToInt32(e.Time * 100 / totalTime);
+            var timeFormat = @"hh\:mm\:ss";
+            var progressText = $"{TimeSpan.FromMilliseconds(e.Time).ToString(timeFormat)} / {TimeSpan.FromMilliseconds(totalTime).ToString(timeFormat)}";
+            BeginInvoke(new Action(() =>
+            {
+                pbMain.Value = progress;
+                lblProgress.Text = progressText;
+            }));
+        }
+
+        private void UpdateDriveInfo(DriveInfo driveInfo)
+        {
+            if (driveInfo == null && currentDriverInfo == null)
+                return;
+            if (driveInfo == null)
+            {
+                onError();
+                return;
+            }
+
+            if (currentDriverInfo == null || currentDriverInfo.Name != driveInfo.Name)
+            {
+                currentDriverInfo = driveInfo;
+                lblUDisk.Text = $"{driveInfo.VolumeLabel} ({driveInfo.Name})";
+                mediaList.Clear();
+                foreach (var musicFileSearchPattern in musicFileSearchPatterns)
+                    foreach (var file in Directory
+                        .GetFiles(driveInfo.Name, musicFileSearchPattern, SearchOption.AllDirectories)
+                        .OrderBy(t => t))
+                        mediaList.Add(file);
+
+                btnPause.Enabled = true;
+                btnPre.Enabled = true;
+                btnNext.Enabled = true;
+
+                playNext();
+            }
+        }
+
+        private void playPre()
+        {
+            BeginInvoke(new Action(() =>
+            {
+                if (mediaList.Count == 0)
+                    return;
+
+                currentMediaIndex--;
+                if (currentMediaIndex < 0)
+                    currentMediaIndex = mediaList.Count - 1;
+
+                play(mediaList[currentMediaIndex]);
+            }));
+        }
+
+        private void play(string file = null)
+        {
+            if (!string.IsNullOrEmpty(file))
+                mediaPlayer.Play(new Media(libvlc, file));
+        }
+
+        private void playNext()
+        {
+            BeginInvoke(new Action(() =>
+                {
+                    if (mediaList.Count == 0)
+                        return;
+
+                    currentMediaIndex++;
+                    if (currentMediaIndex >= mediaList.Count)
+                        currentMediaIndex = 0;
+
+                    play(mediaList[currentMediaIndex]);
+                }));
+        }
+
+        private void timerUDiskCheck_Tick(object sender, EventArgs e)
+        {
+            var driveInfo = DriveInfo.GetDrives().Where(t => t.IsReady && t.DriveType == DriveType.Removable).LastOrDefault();
+            UpdateDriveInfo(driveInfo);
+        }
+
+        private void btnPre_Click(object sender, EventArgs e)
+        {
+            playPre();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            playNext();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            mediaPlayer.Pause();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312;
+            //按快捷键 
+            switch (m.Msg)
+            {
+                case WM_HOTKEY:
+                    switch (m.WParam.ToInt32())
+                    {
+                        case 101:
+                            btnPause.PerformClick();
+                            break;
+                        case 102:
+                            btnPre.PerformClick();
+                            break;
+                        case 103:
+                            btnNext.PerformClick();
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref m);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            HotKey.RegisterHotKey(Handle, 101, HotKey.KeyModifiers.None, Keys.Tab);
+            HotKey.RegisterHotKey(Handle, 102, HotKey.KeyModifiers.None, Keys.Up);
+            HotKey.RegisterHotKey(Handle, 103, HotKey.KeyModifiers.None, Keys.Down);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            HotKey.UnregisterHotKey(Handle, 101);
+            HotKey.UnregisterHotKey(Handle, 102);
+            HotKey.UnregisterHotKey(Handle, 103);
+        }
+    }
+}
